e8c43226e35b06bd9497fe469b6d85d0c318dccc
OceanH <1404939185@qq.com>
2025-08-22 00:42:26 +0800

    feat: init

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import { lazy, Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const Home = lazy(() => import("../pages/Home/index"));
+const Login = lazy(() => import("../pages/Login/index"))
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<div>Loading Header...</div>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: (
+      <Suspense fallback={<div>Loading Header...</div>}>
+        <Login />
+      </Suspense>
+    ),
+  }
+]);
+
+export default router;
