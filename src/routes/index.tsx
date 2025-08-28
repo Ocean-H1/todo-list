ac57df628475c4fc317bb8e785e4e4b346aabb3c
OceanH <1404939185@qq.com>
2025-08-29 00:28:29 +0800

    feat: config router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,31 +3,36 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 
 const Home = lazy(() => import("../pages/Home/index"));
-const Login = lazy(() => import("../pages/Login/index"))
+const Login = lazy(() => import("../pages/Login/index"));
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense fallback={<div>Loading Header...</div>}>
+              <Home />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: (
+        <Suspense fallback={<div>Loading Header...</div>}>
+          <Login />
+        </Suspense>
+      ),
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: (
-          <Suspense fallback={<div>Loading Header...</div>}>
-            <Home />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: (
-      <Suspense fallback={<div>Loading Header...</div>}>
-        <Login />
-      </Suspense>
-    ),
+    basename: process.env.NODE_ENV === "production" ? "/todo-list" : "/",
   }
-]);
+);
 
 export default router;
